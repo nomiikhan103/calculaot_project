f94f7f312fc264e14067673a24a8682044ff082b
nomiikhan103 <69902367+nomiikhan103@users.noreply.github.com>
2024-03-24 15:54:31 +0500

    typescript and java-script files added

---
+++ calculator.js
@@ -0,0 +1,36 @@
+import inquirer from "inquirer";
+const calculation = await inquirer.prompt([
+    {
+        type: "number",
+        name: "fristvalue",
+        message: "Enter first number",
+    },
+    {
+        type: "number",
+        name: "secoundvalue",
+        message: "Enter secound value",
+    },
+    {
+        type: "list",
+        name: "Selectopreators",
+        choices: ["+", "-", "*", "/"],
+    },
+]);
+if (calculation.Selectopreators == "+") {
+    console.log("the sum of two numbers is", calculation.fristvalue + calculation.secoundvalue);
+}
+else if (calculation.Selectopreators == "-") {
+    console.log("the subtraction of two numbers is", calculation.fristvalue - calculation.secoundvalue);
+}
+else if (calculation.Selectopreators == "*") {
+    console.log("the multiplication of two numbers is", calculation.fristvalue * calculation.secoundvalue);
+}
+else if (calculation.Selectopreators == "*") {
+    console.log("the multiplication of two numbers is", calculation.fristvalue * calculation.secoundvalue);
+}
+else if (calculation.Selectopreators == "/") {
+    console.log("the division of two numbers is", calculation.fristvalue * calculation.secoundvalue);
+}
+else {
+    console.log("you have select an invalid opreator");
+}
